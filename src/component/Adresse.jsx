c1cfb026f9b7910bd4bca6d1b6bce8bb52e7a193
Anis114 <93031600+Anis114@users.noreply.github.com>
2022-07-31 13:12:10 +0100

    jsx

---
+++ src/component/Adresse.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Adresse = props => {
+    return (
+        <div>
+            <h2>Rue Raymond Arab N°1 La marsa, Tunis, Tunisie</h2>
+        </div>
+    );
+};
+
+
+export default Adresse;
---
+++ src/component/Fullname.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Fullname = props => {
+    return (
+        <div>
+            <h1>Anis Nawar</h1>
+        </div>
+    );
+};
+
+
+export default Fullname;
